0a96bf1513705d25e04b9f181d4a444e94599457
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-15 13:51:47 -0700

    Develop App

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from 'react-router-dom';
+import BookList from './Components/BookList';
+import BookForm from './Components/BookForm';
+import Nav from './Componenets/Nav';
+import Catogory from './Componenets/Catogories';
+
+
+const App = () => (
+    <>
+        <Router>
+            <Nav />
+            <Switch>
+                <Route exact path="/">
+                    <BookList />
+                    <BookForm />
+                </Route>
+                <Route exact path="/Catogories">
+                    <Catogory />
+                </Route>
+            </Switch>
+        </Router>
+    </>
+);
---
+++ src/Componenets/Catogories.js
@@ -0,0 +1,8 @@
+const Catogory = () => {
+    return (
+        <div>
+            <h1>Under Construction</h1>
+        </div>
+    )
+}
+export default Catogory;
