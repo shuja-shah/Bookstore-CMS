7cfa179b00ca93afbd4a41374fb4698d4bd53c65
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-15 13:37:27 -0700

    Develop Book Form

---
+++ src/Componenets/BookForm.js
@@ -0,0 +1,9 @@
+const BookForm = () => {
+    return (
+        <form>
+                <input type="text" placeholder="Title" />
+                <input type="text" placeholder="Author" />
+                <button type="submit">Add Book</button>
+            </form>
+}
+export default BookForm;
