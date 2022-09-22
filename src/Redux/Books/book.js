cbfe7dabcbede3c3611c5092fbab3fa916968753
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-22 14:54:41 -0700

    Add {Reducer, Actions} To Book.js

---
+++ src/Redux/Books/book.js
@@ -0,0 +1,31 @@
+const addBook = (book) => ({
+  type: 'ADD_BOOK',
+  payload: book,
+});
+
+const removeBook = (book_ID) => ({
+  type: 'REMOVE_BOOK',
+  payload: book_ID,
+});
+
+export { addBook, removeBook };
+const initialState = {
+  books: [],
+};
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_BOOK':
+      return {
+        ...state,
+        books: state.books.concat(action.payload),
+      };
+    case 'REMOVE_BOOK':
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+export default bookReducer;
