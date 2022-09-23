d6e7dce208f98684cfe3b43fb9ae1c6f26d21dee
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-23 04:34:52 -0700

    Define RemoveBook

--- src/Componenets/BookList.js
+++ src/Componenets/BookList.js
@@ -8,6 +8,7 @@ const BookList = () => {
       {books.map((book) => (
         <Book
           key={book.id}
+          id={book.id}
           title={book.title}
           author={book.author}
         />
