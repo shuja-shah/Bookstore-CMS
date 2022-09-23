28592669d95a9b0b98ac37fb3617c5440e52d731
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-23 14:25:30 -0700

    Define Create Delete Display thunks

--- src/Redux/API/api.js
+++ src/Redux/API/api.js
@@ -1,8 +1,5 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0Z0Q9q2Y2QYQ2Y2Q2Y2Q/books',
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
+// export default axios.create({
+//     baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/vK6OLioSURRYt4btr4gO',
+// });
--- src/Redux/Books/book.js
+++ src/Redux/Books/book.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const addBook = (book) => ({
   type: 'ADD_BOOK',
@@ -8,6 +9,38 @@ const addBook = (book) => ({
 const removeBook = (bookId) => ({
   type: 'REMOVE_BOOK',
   payload: bookId,
+});
+
+export const getBooks = createAsyncThunk('GET_BOOKS', async () => {
+  const response = await axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/vK6OLioSURRYt4btr4gO/books');
+  const data = await response.data;
+  const books = Object.keys(data).map((key) => ({
+    id: key,
+    title: data[key][0].title,
+    author: data[key][1].author,
+  }));
+  return books;
+});
+
+export const addBookToAPI = createAsyncThunk('ADD_BOOK_TO_API', async (book) => {
+  await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/vK6OLioSURRYt4btr4gO/books', {
+    method: 'POST',
+    body: JSON.stringify(book),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return book;
+});
+
+export const removeBookFromAPI = createAsyncThunk('REMOVE_BOOK_FROM_API', async (bookId) => {
+  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/vK6OLioSURRYt4btr4gO/books/${bookId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return bookId;
 });
 
 export { addBook, removeBook };
