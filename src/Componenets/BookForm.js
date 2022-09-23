d95f83044eda36c6057f0f25bb107718ada0289d
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-23 04:01:32 -0700

    Define Adding Book

--- src/Componenets/BookForm.js
+++ src/Componenets/BookForm.js
@@ -1,8 +1,37 @@
-const BookForm = () => (
-  <form>
-    <input type="text" placeholder="Title" />
-    <input type="text" placeholder="Author" />
-    <button type="submit">Add Book</button>
-  </form>
-);
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../Redux/Books/book';
+
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmission = (e) => {
+    e.preventDefault();
+    setTitle(e.target[0].value);
+    setAuthor(e.target[1].value);
+  };
+
+  const sendSubmition = (e) => {
+    e.preventDefault();
+    dispatch(addBook(
+      {
+        id: Math.floor(Math.random() * 1000000) + 1,
+        title,
+        author,
+      },
+    ));
+
+    setAuthor('');
+    setTitle('');
+  };
+  return (
+    <form onSubmit={sendSubmition}>
+      <input type="text" placeholder="Title" onChange={handleSubmission} />
+      <input type="text" placeholder="Author" onChange={handleSubmission} />
+      <button type="submit" onClick={sendSubmition}>Add Book</button>
+    </form>
+  );
+};
 export default BookForm;
