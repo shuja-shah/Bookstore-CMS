12ff31a5b9c1536dd87ff22054a75649f2a1f9fe
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-24 05:11:48 -0700

    Implement Requested Changes

--- src/Redux/API/api.js
+++
@@ -1,5 +0,0 @@
-// import axios from 'axios';
-
-// export default axios.create({
-//     baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/vK6OLioSURRYt4btr4gO',
-// });
